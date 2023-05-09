130483a0824e2f1cadb697ef4545b504a8dd2924
long <pdlong578@gmail.com>
2023-05-09 16:59:03 +0700

    add authentication.jwtBear

--- src/Api/Controllers/IdentityController.cs
+++ src/Api/Controllers/IdentityController.cs
@@ -1,6 +1,17 @@
-﻿namespace Api.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
 {
-    public class IdentityController
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
     }
 }
